e896677772b8aa04da82e4a87df65f8496b5f8e7
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-08-26 19:11:22 +0700

    added a second solution method

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,3 +1,79 @@
+// function sendRequest(method, url, dataForPost = null, contentType = null) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(method, url);
+//     xhr.responseType = 'json';
+//     if (contentType !== null) {
+//       xhr.setRequestHeader('Content-Type', contentType);
+//     }
+
+//     xhr.onload = () => {
+//       if (xhr.status >= 400) {
+//         reject(xhr.response);
+//       } else {
+//         resolve(xhr.response);
+//       }
+//     }
+
+//     xhr.onerror = () => {
+//       reject(xhr.response);
+//     }
+
+//     xhr.send(dataForPost);
+//   });
+// }
+
+// const actionsAfterSuccessXhr = () => {
+//   sendRequest('GET', 'https://students.netoservices.ru/nestjs-backend/poll')
+//   .then(response => {
+//     const title = response.data.title;
+//     const answers = response.data.answers;
+
+//     const pollTitle = document.getElementById('poll__title');
+//     const pollAnswers = document.getElementById('poll__answers');
+
+//     pollTitle.insertAdjacentHTML("beforeEnd", `${title}`);
+
+//     answers.forEach(answer => {
+//       pollAnswers.insertAdjacentHTML("beforeEnd", `
+//         <button class="poll__answer">${answer}</button>
+//       `);
+//     });
+
+//     pollAnswers.addEventListener('click', (event) => {
+//       if (event.target.classList.contains('poll__answer')) {
+//         alert('Спасибо, Ваш голос засчитан!');
+//         const answerIndex = response.data.answers.indexOf(event.target.textContent);
+//         const dataForPost = `vote=${response.id}&answer=${answerIndex}`;
+//         sendRequest('POST', 'https://students.netoservices.ru/nestjs-backend/poll', dataForPost, 'application/x-www-form-urlencoded')
+//           .then(response => {
+//             const pollPersentages = document.getElementById('poll__percentages');
+//             const arrOfObjectsStat = response.stat;
+
+//             let sumOfVotes = 0;
+//             arrOfObjectsStat.forEach(objectStat => sumOfVotes += objectStat.votes);
+
+//             arrOfObjectsStat.forEach(objectStat => {
+//               const answerPercentage = ((objectStat.votes / sumOfVotes) * 100).toFixed(2);
+//               pollPersentages.insertAdjacentHTML("beforeEnd", `
+//                 <p class="poll__percentage">
+//                   ${objectStat.answer} ─ <span class=".poll__percentage__bold">${answerPercentage}%</span>
+//                 </p>
+//               `);
+//             });
+//           })
+//           .catch(error => console.log(error));
+//       }
+//     })
+//   })
+//   .catch(error => console.log(error));
+  
+// }
+
+// actionsAfterSuccessXhr();
+
+
+// ----------------------------------------------------------------------------------
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
@@ -64,7 +140,7 @@ const requestForPercentageOfVotes = (answerIndex, response) => {
         `);
       });
     } else {
-      console.error('Ошибка загрузки: ', xhr.statusText);
+      console.error('Ошибка загрузки: ', xhr.response);
     }
   };
 
